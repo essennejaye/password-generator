0e1435eeadba87baa086d801c9ed3f89a90eb717
essennejaye <sjeffers02@gmail.com>
2020-06-12 12:51:54 -0700

    get user input for length and character selections

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
 // Assignment code here
-window.alert("Welcome to the Password Generator");
+function getPasswordLength() {
+  var passwordLength = prompt("Please enter a password length between 8 and 128 characters");
+  if (isNaN(passwordLength) ||
+    passwordLength < 8 ||
+    passwordLength > 128) {
+    alert("Invalid selection");
+    getPasswordLength();
+  } else {
+    return passwordLength;
+  }
+}
+function getUserSelection() {
+  var userSelection = [];
+  var s = confirm("Do you want to include lowercase letters in your password?");
+  userSelection.push(s);
+  var s2 = confirm("Do you want to include uppercase letters in your password?");
+  userSelection.push(s2);
+  var s3 = confirm("Do you want to include numbers in your password?");
+  userSelection.push(s3);
+  var s4 = confirm("Do you want to include special characters in your password?");
+  userSelection.push(s4);
+  return userSelection;
+}
 
+function generatePassword() {
+var length = getPasswordLength();
+var choice = getUserSelection();
+}
 
 
 // Get references to the #generate element
@@ -17,3 +43,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generatePassword();
